10669eb76b3a09d163d45c845a15ed5d37ffcd6f
pipebxeno91 <54948007+pipebxeno91@users.noreply.github.com>
2019-09-05 15:45:32 -0400

    mis funciones

---
+++ ERP/misFunciones/misFunciones/Class1.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+Imports ExcelDna.Integration
+Imports ExcelDna.ComInterop
+Imports System.Runtime.InteropServices
+
+<ComVisible(True)>
+<ClassInterface(ClassInterfaceType.AutoDispatch)>
+<ProgId("excelymas")>
+Public Class Class1
+    <ExcelFunction(Description:="Mi primera función UDF en .NET")>
+    Public Function DimeHola(nombre As String) As String
+        Return "Hola " & nombre
+    End Function
+
+    <ExcelFunction(Description:="Función para convertir números a letras")>
+    Public Function ConvertirNumerosALetras(num As Integer) As String
+        Dim texto As String
+        Dim nUnidades As Integer
+        Dim cUnidades() As String = {"cero", "uno", "dos", "tres", "cuatro", "cinco",
+                                        "seis", "siete", "ocho", "nueve"}
+        nUnidades = num
+
+        texto = cUnidades(nUnidades)
+
+        ConvertirNumerosALetras = texto
+
+    End Function
+End Class
+
+Public Class AddIn
+    Implements IExcelAddIn
+
+    Public Sub AutoOpen() Implements IExcelAddIn.AutoOpen
+        ComServer.DllRegisterServer()
+    End Sub
+
+    Public Sub AutoClose() Implements IExcelAddIn.AutoClose
+        ComServer.DllRegisterServer()
+    End Sub
+End Class
---
+++ ERP/misFunciones/misFunciones/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports ExcelDna.Integration
+Public Module Module1
+    <ExcelFunction(Description:="Mi primera función UDF en .NET")>
+    Public Function DimeHola(nombre As String) As String
+        Return "Hola " & nombre
+    End Function
+
+    <ExcelFunction(Description:="Función para convertir números a letras")>
+    Public Function ConvertirNumerosALetras(num As Integer) As String
+        Dim texto As String
+        Dim nUnidades As Integer
+        Dim cUnidades() As String = {"cero", "uno", "dos", "tres", "cuatro", "cinco",
+                                        "seis", "siete", "ocho", "nueve"}
+        nUnidades = num
+
+        texto = cUnidades(nUnidades)
+
+        ConvertirNumerosALetras = texto
+
+    End Function
+End Module
